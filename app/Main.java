2378bdfd719d9d356459b7c76a966f545c970f5c
Federico Anastasi <fede_3775@hotmail.com>
2019-06-20 00:10:36 -0300

    Boceto del metodo main

---
+++ app/Main.java
@@ -0,0 +1,45 @@
+package app;
+
+import Modelos.Jugador;
+import Modelos.Mazo;
+import Modelos.Tablero;
+
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import Extras.JsonUtiles;
+
+public class Main {
+
+	public static void main(String[] args) {		
+		
+		JSONArray mazoJ1=null;
+		
+		try {
+			mazoJ1 = new JSONArray(JsonUtiles.leer("mazoJ1.json"));
+		} catch (JSONException e) {
+			System.out.println("Archivo json mal formateado");
+			e.printStackTrace();
+		}
+		JSONArray mazoJ2=null;
+		
+		try {
+			mazoJ2 = new JSONArray(JsonUtiles.leer("mazoJ2.json"));
+		} catch (JSONException e) {
+			System.out.println("Archivo json mal formateado");
+			e.printStackTrace();
+		}
+		
+		
+		Jugador j1 = new Jugador( new Mazo(mazoJ1));
+		Jugador j2 = new Jugador( new Mazo(mazoJ2));
+		
+		Tablero t= new Tablero(j1,j2);
+		
+		
+		t.jugar();
+		
+	}
+
+}
