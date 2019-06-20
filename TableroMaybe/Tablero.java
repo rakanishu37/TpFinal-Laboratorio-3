3fd46985b9972ecf1b35eecdcdd8b0fe16466d8b
luissacha <44303425+luissacha@users.noreply.github.com>
2019-06-20 17:33:43 -0300

    vamo a calamarlo
    upsi

--- TableroMaybe/Tablero.java
+++ TableroMaybe/Tablero.java
@@ -1,66 +1,66 @@
 package TableroMaybe;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.Image;
 import javax.swing.ImageIcon;
 
 import javax.swing.*;
 
-public class Tablero extends JFrame implements ActionListener
+public class Tablero extends JFrame //implements ActionListener
 {
-	private JLabel label1, labelimagen, imagensola;
-	private JButton boton1, boton2;
+	public static final int DIM_POKE_X = 240;
+	public static final int DIM_POKE_Y = 330;
+	
+	private JLabel iconoPokemonAtacante, iconoPokemonDefensor;
+	private JButton boton1, boton2, boton3, boton4;
 	
 	private Tablero()
 	{
+		
+		setTitle("POKEMON TCG 3.0 viturro ATR perro.");
 		setLayout(null);
-		label1 = new JLabel("Zona de Prueba");
-		label1.setBounds(20,20,200,100);
-		add(label1);
 		
-		boton1 = new JButton("cerrar");
-		boton1.setBounds(600, 50, 200, 50);
+		boton1 = new JButton("Aca iria mi ataque 2 atacante");
+		boton1.setBounds(104, 610, 230, 70);										// USAR ESTASS MEDIDAS DE REFERENCIA										
 		add(boton1);
-		boton1.addActionListener(this);
 		
-		boton2 = new JButton("imprime algo");
-		boton2.setBounds(600, 110, 200, 50);
-		add(boton2);
-		boton2.addActionListener(this);
-		setTitle("EL POKEMON TCG PAPA");
+		boton2 = new JButton("Aca iria mi ataque 1 atacante");
+		boton2.setBounds(104, 530, 230, 70);
+		add(boton2);		
 		
-		ImageIcon imagenV = new ImageIcon(getClass().getResource("/Imagenes/Venusaur.png"));
+		boton3 = new JButton("Aca iria mi ataque 2 defensor");
+		boton3.setBounds(104, 250, 230, 70);										
+		add(boton3);
 		
-		Image imagen = imagenV.getImage();
-		ImageIcon iconoEscalado = new ImageIcon(imagen.getScaledInstance(220, 300, Image.SCALE_SMOOTH));
-			
-		labelimagen = new JLabel();
-		labelimagen.setBounds(100, 100, 220, 300);
-		labelimagen.setIcon(iconoEscalado);
-		add(labelimagen);
+		boton4 = new JButton("Aca iria mi ataque 1 defensor");
+		boton4.setBounds(104, 170, 230, 70);										
+		add(boton4);
+
+		ImageIcon rawActivoA = new ImageIcon(getClass().getResource("/Imagenes/Venusaur.png"));
+		Image imagenActivoA = rawActivoA.getImage();
+		ImageIcon iconoEscaladoAtacante = new ImageIcon(imagenActivoA.getScaledInstance(DIM_POKE_X, DIM_POKE_Y, Image.SCALE_SMOOTH));
+		iconoPokemonAtacante = new JLabel();
+		iconoPokemonAtacante.setBounds(100, 375, DIM_POKE_X, DIM_POKE_Y);							//SET DE FUNCIONES PARA IMAGEN ATACANTE
+		iconoPokemonAtacante.setIcon(iconoEscaladoAtacante);
+		add(iconoPokemonAtacante);
+	
+		
+		ImageIcon rawActivoD = new ImageIcon(getClass().getResource("/Imagenes/Venusaur.png"));
+		Image imagenActivoD = rawActivoD.getImage();
+		ImageIcon iconoEscaladoDefensor = new ImageIcon(imagenActivoD.getScaledInstance(DIM_POKE_X, DIM_POKE_Y, Image.SCALE_SMOOTH));
+		iconoPokemonDefensor = new JLabel();
+		iconoPokemonDefensor.setBounds(100, 25, DIM_POKE_X, DIM_POKE_Y);							//SET DE FUNCIONES PARA IMAGEN DEFENSOR
+		iconoPokemonDefensor.setIcon(iconoEscaladoDefensor);
+		add(iconoPokemonDefensor);
+		
+		
 		
 	}
-	
-	@Override
-	public void actionPerformed(ActionEvent e) 
-	{
-		if(e.getSource() == boton1)
-		{
-																//			ACA TENDIRAN QUE IR LOS METODOS, QUE LLAME
-			System.exit(0);										// ESTO HACE QUE CIERRE EL PROGRAMA
-		}
 		
-		if(e.getSource() == boton2)
-		{
-			label1.setText("apretaste el boton 2 wachin");		// SETTEXT CAMBIA EL TEXTO QUE ESTA EN EL LABEL					// CON ESTO REFRESCARIAMOS LA VIDA POR EJEMPLO
-		}
-	}
-	
+		
 	public static void main (String args[])
 	{
 		Tablero tablero = new Tablero();
-		tablero.setBounds(200, 100, 1024, 768);					// LAS DIMENSIONES DEL TABLERO: DISTANCIA EN X, DISTANCIA EN Y, ANCHO, LARGO.
+		tablero.setBounds(200, 100, 450, 768);					// LAS DIMENSIONES DEL TABLERO: DISTANCIA EN X, DISTANCIA EN Y, ANCHO, LARGO.
 		tablero.setVisible(true);								// PARA QUE SE VEA LA INTERFAZ.
 		tablero.setLocationRelativeTo(null);					// PARA QUE LA INTERFAZ ESTE EN EL MEDIO CUANDO SE CREA, IGNORA X e Y
 		tablero.setResizable(false);							// PARA QUE NO PUEDA MODIFICAR LAS DIMENSIONES
